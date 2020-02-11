c75c6d5b1bef9927fdacf72c0f38d9fe0eaf9107
liwei1dao <liwei1dao@iCloud.com>
2020-02-11 11:52:14 +0800

    添加对外接口

--- sys/sqls/core.go
+++ sys/sqls/core.go
@@ -1,6 +1,10 @@
 package sqls
 
 import (
+	"database/sql"
+
+	_ "github.com/denisenkom/go-mssqldb"
+
 	"github.com/liwei1dao/lego/core"
 )
 
@@ -13,4 +17,12 @@ func OnInit(s core.IService, opt ...Option) (err error) {
 	service = s
 	sqlserver, err = newSqlServer(opt...)
 	return
+}
+
+func ExecContext(query string, args ...interface{}) (err error) {
+	return sqlserver.ExecContext(query, args...)
+}
+
+func QueryContext(query string, args ...interface{}) (data *sql.Rows, err error) {
+	return sqlserver.QueryContext(query, args...)
 }
