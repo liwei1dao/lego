14835be734eabe90b9e68501aef3a8928b1da007
liwei1dao <liwei1dao@iCloud.com>
2020-03-02 21:20:04 +0800

    移除必须要的接口

--- utils/utils.go
+++ utils/utils.go
@@ -7,7 +7,6 @@ import (
 	"reflect"
 	"strings"
 	"time"
-	"unsafe"
 )
 
 func GetApplicationDir() (ApplicationDir string) {
@@ -133,14 +132,4 @@ func copyRecursive(src, dst reflect.Value) {
 	default:
 		dst.Set(src)
 	}
-}
-
-func Str2Bytes(s string) []byte {
-	x := (*[2]uintptr)(unsafe.Pointer(&s))
-	h := [3]uintptr{x[0], x[1], x[1]}
-	return *(*[]byte)(unsafe.Pointer(&h))
-}
-
-func Bytes2Str(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
 }
