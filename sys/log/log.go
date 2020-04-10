b4bd5ac25d8774d82626c608c0a872dd7dbf3159
liwei1dao <2211068034@qq.com>
2020-04-10 12:15:37 +0800

    修改日志默认对象名

--- sys/log/log.go
+++ sys/log/log.go
@@ -2,27 +2,19 @@ package log
 
 import (
 	"fmt"
-	"github.com/liwei1dao/lego/core"
 
-	"go.uber.org/zap"
+	"github.com/liwei1dao/lego/core"
 )
 
 var (
-	service core.IService
-	log     Ilog
+	service    core.IService
+	defaultlog Ilog
 )
 
-func init() {
-	tlog, _ := zap.NewDevelopment(zap.AddCaller(), zap.AddCallerSkip(2))
-	log = &Logger{
-		tlog: tlog,
-		log:  tlog.Sugar(),
-	}
-}
 func OnInit(s core.IService, opt ...Option) (err error) {
 	service = s
 	opt = append(opt, SetFileName(fmt.Sprintf("%sbin/log/%s/%s.log", s.GetWorkPath(), s.GetId(), s.GetType())))
-	log, err = newLog(opt...)
+	defaultlog, err = newLog(opt...)
 	return
 }
 func NewILog(opt ...Option) (log Ilog, err error) {
@@ -30,15 +22,15 @@ func NewILog(opt ...Option) (log Ilog, err error) {
 	return
 }
 
-func Debug(msg string, fields ...Field)      { log.Debug(msg, fields...) }
-func Info(msg string, fields ...Field)       { log.Info(msg, fields...) }
-func Warn(msg string, fields ...Field)       { log.Warn(msg, fields...) }
-func Error(msg string, fields ...Field)      { log.Error(msg, fields...) }
-func Panic(msg string, fields ...Field)      { log.Panic(msg, fields...) }
-func Fatal(msg string, fields ...Field)      { log.Fatal(msg, fields...) }
-func Debugf(format string, a ...interface{}) { log.Debugf(format, a...) }
-func Infof(format string, a ...interface{})  { log.Infof(format, a...) }
-func Warnf(format string, a ...interface{})  { log.Warnf(format, a...) }
-func Errorf(format string, a ...interface{}) { log.Errorf(format, a...) }
-func Panicf(format string, a ...interface{}) { log.Panicf(format, a...) }
-func Fatalf(format string, a ...interface{}) { log.Fatalf(format, a...) }
+func Debug(msg string, fields ...Field)      { defaultlog.Debug(msg, fields...) }
+func Info(msg string, fields ...Field)       { defaultlog.Info(msg, fields...) }
+func Warn(msg string, fields ...Field)       { defaultlog.Warn(msg, fields...) }
+func Error(msg string, fields ...Field)      { defaultlog.Error(msg, fields...) }
+func Panic(msg string, fields ...Field)      { defaultlog.Panic(msg, fields...) }
+func Fatal(msg string, fields ...Field)      { defaultlog.Fatal(msg, fields...) }
+func Debugf(format string, a ...interface{}) { defaultlog.Debugf(format, a...) }
+func Infof(format string, a ...interface{})  { defaultlog.Infof(format, a...) }
+func Warnf(format string, a ...interface{})  { defaultlog.Warnf(format, a...) }
+func Errorf(format string, a ...interface{}) { defaultlog.Errorf(format, a...) }
+func Panicf(format string, a ...interface{}) { defaultlog.Panicf(format, a...) }
+func Fatalf(format string, a ...interface{}) { defaultlog.Fatalf(format, a...) }
