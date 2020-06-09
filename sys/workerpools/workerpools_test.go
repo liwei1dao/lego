6c2579107e8c0cfd80712a379f89040f947db7a1
liwei1dao <2211068034@qq.com>
2020-06-09 12:26:38 +0800

    上传工作池测试代码

--- sys/workerpools/workerpools_test.go
+++ sys/workerpools/workerpools_test.go
@@ -10,10 +10,11 @@ func Test_workerpools(t *testing.T) {
 	pools, _ := NewTaskPools(SetMaxWorkers(1), SetTaskTimeOut(time.Second*4))
 	go func() {
 		pools.Submit(func(ctx context.Context, cancel context.CancelFunc, agrs ...interface{}) {
+			agr0 := agrs[0].(string)
 			time.Sleep(time.Second * 6)
-			t.Logf("liwei2dao")
+			t.Logf(agr0)
 			cancel()
-		})
+		}, "liwei1dao")
 	}()
 
 	// go func() {
@@ -27,9 +28,10 @@ func Test_workerpools(t *testing.T) {
 	go func() {
 		time.Sleep(time.Second * 1)
 		pools.Submit(func(ctx context.Context, cancel context.CancelFunc, agrs ...interface{}) {
-			t.Logf("liwei1dao")
+			agr0 := agrs[0].(string)
+			t.Logf(agr0)
 			cancel()
-		})
+		}, "liwei2dao")
 	}()
 
 	time.Sleep(time.Second * 10)
