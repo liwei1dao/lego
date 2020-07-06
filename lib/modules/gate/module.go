10b9564300b16261de31da8463c3e9790d7a77ce
liwei1dao <2211068034@qq.com>
2020-07-06 17:21:51 +0800

    修复上传代码

--- lib/modules/gate/module.go
+++ lib/modules/gate/module.go
@@ -22,8 +22,8 @@ func (this *Gate) GetLocalRouteMgrComp() ILocalRouteMgrComp {
 }
 
 //需重构处理  内部函数为重构代码d
-func (this *Gate) RegisterRemoteRoute(comId uint16, sId string) (result string, err string) {
-	result, err = this.RemoteRouteMgrComp.RegisterRoute(comId, sId)
+func (this *Gate) RegisterRemoteRoute(comId uint16, sId, sType string) (result string, err string) {
+	result, err = this.RemoteRouteMgrComp.RegisterRoute(comId, sId, sType)
 	return
 }
 func (this *Gate) RegisterLocalRoute(comId uint16, f func(session core.IUserSession, msg proto.IMessage) (code core.ErrorCode, err string)) {
