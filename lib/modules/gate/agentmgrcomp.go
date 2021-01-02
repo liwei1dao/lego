22b24aac18b6a8f912b0102773e8d7d167b605ee
liwei1dao <2211068034@qq.com>
2021-01-02 10:37:28 +0800

    上传网关代码

--- lib/modules/gate/agentmgrcomp.go
+++ lib/modules/gate/agentmgrcomp.go
@@ -28,6 +28,7 @@ func (this *AgentMgrComp) Destroy() (err error) {
 		agent := v.(IAgent)
 		agent.OnClose()
 	}
+	this.Agents.DeleteAll()
 	return
 }
 
@@ -56,6 +57,7 @@ func (this *AgentMgrComp) SendMsg(aId string, msg proto.IMessage) (result int, e
 }
 func (this *AgentMgrComp) Close(aId string) (result string, err string) {
 	agent := this.Agents.Get(aId)
+	defer this.Agents.Delete(aId)
 	if agent == nil {
 		err = fmt.Sprintf("No agent found " + aId)
 		return
