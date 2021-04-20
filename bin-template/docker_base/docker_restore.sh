5510c46bfedb608391007cef5a4ea2f826ed3ec8
liwei1dao <liwei1dao@iCloud.com>
2021-04-20 21:03:00 +0800

    移除模版文件夹

--- bin-template/docker_base/docker_restore.sh
+++
@@ -1,6 +0,0 @@
-# 还原docker 数据卷
-tar -zxvf ~/docker-backup/volume.tgz -C ~/docker-backup
-docker volume rm mongodata
-docker volume rm redisdata
-docker run --rm -it -v /var/lib/docker:/docker -v ~/docker-backup/docker/volumes:/volume-backup busybox cp -r /volume-backup/mongodata /docker/volumes
-docker run --rm -it -v /var/lib/docker:/docker -v ~/docker-backup/docker/volumes:/volume-backup busybox cp -r /volume-backup/redisdata /docker/volumes
