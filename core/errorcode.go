451c1226e1a7838e9c057e0c0d696aebee71b451
liwei1dao <liwei1dao@gmail.com>
2021-06-08 09:54:22 +0800

    上传错误码

--- core/errorcode.go
+++ core/errorcode.go
@@ -3,13 +3,14 @@
 import "fmt"
 
 const (
-	ErrorCode_Success               ErrorCode = 0  //成功
-	ErrorCode_NoFindService         ErrorCode = 10 //没有找到远程服务器
-	ErrorCode_RpcFuncExecutionError ErrorCode = 11 //Rpc方法执行错误
-	ErrorCode_CacheReadError        ErrorCode = 12 //缓存读取失败
-	ErrorCode_SqlExecutionError     ErrorCode = 13 //数据库执行错误
-	ErrorCode_ReqParameterError     ErrorCode = 14 //请求参数错误
-	ErrorCode_SignError             ErrorCode = 15 //签名错误
+	ErrorCode_Success                 ErrorCode = 0  //成功
+	ErrorCode_NoFindService           ErrorCode = 10 //没有找到远程服务器
+	ErrorCode_RpcFuncExecutionError   ErrorCode = 11 //Rpc方法执行错误
+	ErrorCode_CacheReadError          ErrorCode = 12 //缓存读取失败
+	ErrorCode_SqlExecutionError       ErrorCode = 13 //数据库执行错误
+	ErrorCode_ReqParameterError       ErrorCode = 14 //请求参数错误
+	ErrorCode_SignError               ErrorCode = 15 //签名错误
+	ErrorCode_InsufficientPermissions ErrorCode = 16 //权限不足
 	//gate
 	ErrorCode_NoRoute                  ErrorCode = 201 //没有路由
 	ErrorCode_LocalRouteExecutionError ErrorCode = 202 //本地路由执行错误
@@ -23,6 +24,7 @@ var ErrorCodeMsg = map[ErrorCode]string{
 	ErrorCode_SqlExecutionError:        "数据库执行错误",
 	ErrorCode_ReqParameterError:        "请求参数错误",
 	ErrorCode_SignError:                "签名错误",
+	ErrorCode_InsufficientPermissions:  "权限不足",
 	ErrorCode_NoRoute:                  "没有路由",
 	ErrorCode_LocalRouteExecutionError: "本地路由执行错误",
 }
