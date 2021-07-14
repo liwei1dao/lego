b6d91a7663585dcb0338b37e23c3c4ee8f0ee552
liwei1dao <liwei1dao@gmail.com>
2021-07-14 19:45:41 +0800

    添加kafka 测试代码

--- sys/kafka/sys_test.go
+++ sys/kafka/sys_test.go
@@ -1,1 +1,25 @@
 package kafka
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Shopify/sarama"
+)
+
+func Test_sys(t *testing.T) {
+	if err := OnInit(map[string]interface{}{
+		"StartType": All,
+		"Hosts":     []string{"49.235.111.75:9092"},
+		"Topics":    []string{"test"},
+		"GroupId":   "test-c1",
+		"ClientID":  "send",
+	}); err != nil {
+		fmt.Printf("start sys err:%v", err)
+	} else {
+		Syncproducer_SendMessage(&sarama.ProducerMessage{
+			Topic: "send",
+			Value: sarama.StringEncoder("asdasd"),
+		})
+	}
+}
