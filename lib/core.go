5c3cc9b9cb738c6d3ed09e93f4e8d2b8ba659416
liwei1dao <liwei1dao@gmail.com>
2022-02-07 14:31:56 +0800

    上传注释优化

--- lib/core.go
+++ lib/core.go
@@ -9,10 +9,10 @@ const (
 )
 
 const (
-	SM_GateModule    core.M_Modules = "SM_GateModule"    //gate模块
-	SM_HttpModule    core.M_Modules = "SM_HttpModule"    //hhtp模块
-	SM_MonitorModule core.M_Modules = "SM_MonitorModule" //monitor模块
-	SM_ConsoleModule core.M_Modules = "SM_ConsoleModule" //cnsole模块
-	SM_LiveModule    core.M_Modules = "SM_LiveModule"    //live模块
-	SM_CollyModule   core.M_Modules = "SM_CollyModule"   //colly模块
+	SM_GateModule    core.M_Modules = "SM_GateModule"    //gate模块 	网关服务模块
+	SM_HttpModule    core.M_Modules = "SM_HttpModule"    //hhtp模块		web服务模块
+	SM_MonitorModule core.M_Modules = "SM_MonitorModule" //monitor模块	监控模块
+	SM_ConsoleModule core.M_Modules = "SM_ConsoleModule" //cnsole模块	控制台模块
+	SM_LiveModule    core.M_Modules = "SM_LiveModule"    //live模块		流媒体模块
+	SM_CollyModule   core.M_Modules = "SM_CollyModule"   //colly模块	爬虫模块
 )
