fab3551369b50d02ba7af35064d88070c479761f
liwei1dao <liwei1dao@gmail.com>
2022-08-17 18:48:38 +0800

    上传阿里云sdk服务参数说明

--- sys/sdks/aliyun/sts/options.go
+++ sys/sdks/aliyun/sts/options.go
@@ -9,18 +9,21 @@ type Options struct {
 	AccessKeySecret string
 }
 
+//注册点 cn-shenzhen
 func SetRegionId(v string) Option {
 	return func(o *Options) {
 		o.RegionId = v
 	}
 }
 
+//头像->访问控制->用户->用户 AccessKey
 func SetAccessKeyId(v string) Option {
 	return func(o *Options) {
 		o.AccessKeyId = v
 	}
 }
 
+//头像->访问控制->用户->用户 AccessKeySecret
 func SetAccessKeySecret(v string) Option {
 	return func(o *Options) {
 		o.AccessKeySecret = v
