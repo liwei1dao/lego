4538c5911eccaa7e0da4fd40947a1a3b428910d2
liwei1dao <liwei1dao@gmail.com>
2022-08-29 10:34:24 +0800

    上传敏感词个过滤系统

---
+++ sys/wordguard/options.go
@@ -0,0 +1,53 @@
+package wordguard
+
+import (
+	"errors"
+
+	"github.com/liwei1dao/lego/sys/log"
+	"github.com/liwei1dao/lego/utils/mapstructure"
+)
+
+type Option func(*Options)
+type Options struct {
+	Debug bool //日志是否开启
+	Log   log.Ilogf
+}
+
+func SetDebug(v bool) Option {
+	return func(o *Options) {
+		o.Debug = v
+	}
+}
+
+func SetLog(v log.Ilogf) Option {
+	return func(o *Options) {
+		o.Log = v
+	}
+}
+
+func newOptions(config map[string]interface{}, opts ...Option) (options *Options, err error) {
+	options = &Options{}
+	if config != nil {
+		mapstructure.Decode(config, &options)
+	}
+	for _, o := range opts {
+		o(options)
+	}
+
+	if options.Log = log.NewTurnlog(options.Debug, log.Clone("sys.Blockcache", 2)); options.Log == nil {
+		err = errors.New("log is nil")
+	}
+
+	return
+}
+
+func newOptionsByOption(opts ...Option) (options *Options, err error) {
+	options = &Options{}
+	for _, o := range opts {
+		o(options)
+	}
+	if options.Log = log.NewTurnlog(options.Debug, log.Clone("sys.Blockcache", 2)); options.Log == nil {
+		err = errors.New("log is nil")
+	}
+	return
+}
