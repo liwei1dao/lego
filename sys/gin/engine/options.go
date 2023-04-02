25d64d4f441f51d3664c35cd46fb54c3d25d7168
liwei1dao <liwei1dao@gmail.com>
2023-04-02 11:35:39 +0800

    同步框架代码

--- sys/gin/engine/options.go
+++ sys/gin/engine/options.go
@@ -6,8 +6,9 @@ import (
 
 type Option func(*Options)
 type Options struct {
-	MultipartMemory int64 //文件上传最大尺寸
-	Log             log.ILogger
+	MultipartMemory       int64 //文件上传最大尺寸
+	RedirectTrailingSlash bool
+	Log                   log.ILogger
 }
 
 func SetMultipartMemory(v int64) Option {
@@ -15,6 +16,20 @@ func SetMultipartMemory(v int64) Option {
 		o.MultipartMemory = v
 	}
 }
+
+/*
+	如果当前路由无法匹配，但启用自动重定向
+	带有（不带）尾部斜杠的路径的处理程序存在。
+	例如，如果 /foo/ 被请求，但路由只存在于 /foo，则
+	对于 GET 请求，客户端被重定向到 /foo，http 状态码为 301
+	对于所有其他请求方法，则为 307。
+*/
+func SetRedirectTrailingSlash(v bool) Option {
+	return func(o *Options) {
+		o.RedirectTrailingSlash = v
+	}
+}
+
 
 func SetLog(log log.ILogger) Option {
 	return func(o *Options) {
