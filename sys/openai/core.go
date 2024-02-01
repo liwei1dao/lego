d755e5bd9db525f7a4c9a8f8ade83e2b04cda10e
liwei1dao <liwei1dao@gmail.com>
2024-02-01 13:39:25 +0800

    上传openai接口

--- sys/openai/core.go
+++ sys/openai/core.go
@@ -1,8 +1,17 @@
 package openai
 
+import (
+	"context"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
 type (
 	ISys interface {
-		SendReq(content string) (results string, err error)
+		//非流式
+		SendReq(ctx context.Context, content string) (results string, err error)
+		//流式数据回应
+		SendReqByStream(ctx context.Context, content string) (stream *openai.ChatCompletionStream, err error)
 	}
 )
 
@@ -20,6 +29,6 @@ func NewSys(option ...Option) (sys ISys, err error) {
 	return
 }
 
-func SendReq(content string) (results string, err error) {
-	return defsys.SendReq(content)
+func SendReq(ctx context.Context, content string) (results string, err error) {
+	return defsys.SendReq(ctx, content)
 }
--- sys/openai/options.go
+++ sys/openai/options.go
@@ -1,35 +1,69 @@
 package openai
 
 import (
+	"github.com/liwei1dao/lego/sys/log"
 	"github.com/liwei1dao/lego/utils/mapstructure"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type Option func(*Options)
 type Options struct {
-	Token string
+	Token string //密钥
+	Model string //使用模型
+	Debug bool   //日志是否开启
+	Log   log.ILogger
 }
 
+//设置密钥
 func SetToken(v string) Option {
 	return func(o *Options) {
 		o.Token = v
 	}
 }
 
-func newOptions(config map[string]interface{}, opts ...Option) Options {
-	options := Options{}
+//设置模型
+func SetModel(v string) Option {
+	return func(o *Options) {
+		o.Model = v
+	}
+}
+
+func SetDebug(v bool) Option {
+	return func(o *Options) {
+		o.Debug = v
+	}
+}
+func SetLog(v log.ILogger) Option {
+	return func(o *Options) {
+		o.Log = v
+	}
+}
+
+func newOptions(config map[string]interface{}, opts ...Option) *Options {
+	options := &Options{
+		Model: openai.GPT3Dot5Turbo,
+	}
 	if config != nil {
 		mapstructure.Decode(config, &options)
 	}
 	for _, o := range opts {
-		o(&options)
+		o(options)
+	}
+	if options.Log == nil {
+		options.Log = log.NewTurnlog(options.Debug, log.Clone("sys.openai", 3))
 	}
 	return options
 }
 
-func newOptionsByOption(opts ...Option) Options {
-	options := Options{}
+func newOptionsByOption(opts ...Option) *Options {
+	options := &Options{
+		Model: openai.GPT3Dot5Turbo,
+	}
 	for _, o := range opts {
-		o(&options)
+		o(options)
+	}
+	if options.Log == nil {
+		options.Log = log.NewTurnlog(options.Debug, log.Clone("sys.openai", 3))
 	}
 	return options
 }
