094abb55ee7652ce01b8dcb7bcae0788a5ca1ee9
liwei1dao <liwei1dao@gmail.com>
2024-08-03 17:57:38 +0800

    上传lego代码

--- base/core.go
+++ base/core.go
@@ -18,17 +18,9 @@ type ISingleService interface {
 	core.IService
 }
 
-type IClusterServiceBase interface {
-	core.IService
-	GetTag() string               //获取集群标签
-	GetCategory() core.S_Category //服务类别 例如游戏服
-	GetRpcId() string             //获取rpc通信id
-	GetPreWeight() float64        //集群服务负载值 暂时可以不用理会
-	SetPreWeight(weight int32)    //设置服务器权重
-}
-
 type IClusterService interface {
-	IClusterServiceBase
+	core.IService
+	GetTag() string //获取集群标签
 	Register(rcvr interface{}) (err error)
 	RegisterFunction(fn interface{}) (err error)
 	RegisterFunctionName(name string, fn interface{}) (err error)
@@ -38,7 +30,8 @@ type IClusterService interface {
 }
 
 type IRPCXService interface {
-	IClusterServiceBase
+	core.IService
+	GetTag() string //获取集群标签
 	RegisterFunction(fn interface{}) (err error)
 	RegisterFunctionName(name string, fn interface{}) (err error)
 	RpcCall(ctx context.Context, servicePath string, serviceMethod string, args interface{}, reply interface{}) (err error)
